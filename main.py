86b4313c55184596657c4e4f075dcd7c0f6e792c
tdude <trevorguanhao@gmail.com>
2019-04-21 13:24:54 -0400

    Update main.py
    the physics work if the car is moving in a straight line.
    
    i wished i used numpy

--- main.py
+++ main.py
@@ -12,9 +12,10 @@ def magnitude(vector):
 class Car:
     possible_states = ("do_nothing", "gas", "brake", "turn_right", "turn_left", "turn_right+gas", "turn_left+gas")
 
-    engine_force = 89484 # Watts
-    drag_const = 0
-    rr_const = 0 # Rolling resistance constant.
+    max_engine_force = 5000 # N
+    drag_const = 0.4257
+    rr_const = 12.8 # Rolling resistance constant.
+    mass = 1000
 
 
     def __init__(self, canvas):
@@ -27,6 +28,7 @@ class Car:
             self.canvas.create_line(290, 280, 310, 280), # Top Hor
             self.canvas.create_line(290, 320, 310, 320), # Bot Hor
         ]
+
         # Calculate car center coordinates (where the car's "vision" lines originate from)
         self.car_centerX = (canvas.coords(self.car[2])[0] + canvas.coords(self.car[2])[2]) / 2
         self.car_centerY = (canvas.coords(self.car[0])[1] + canvas.coords(self.car[0])[3]) / 2
@@ -39,12 +41,26 @@ class Car:
         self.state = Car.possible_states[0]
         self.orientation = 90
         self.u = [cos(self.orientation), sin(self.orientation)] # Unit vector for the orientation of the car.
+        self.engine_force = 0
         self.velocity = [0, 0] # The car is at rest.
 
         # Forces and physics stuff.
-        self.f_traction = [i * Car.engine_force for i in self.u] # self.u * Car.engine_force
-        self.f_drag = Car.drag_const * self.velocity * magnitude(self.velocity)
-        self.f_rr = [i * -Car.rr_const for i in self.velocity]
+        self.f_traction = [i * self.engine_force for i in self.u] # self.u * Car.engine_force
+        self.f_drag = [i * -Car.drag_const * magnitude(self.velocity) for i in self.velocity] # drag_const * acceleration * |acceleration|
+        self.f_rr = [i * -Car.rr_const for i in self.velocity] # velocity * -rolling resistance
+        self.acceleration = [(self.f_traction[i] + self.f_drag[i] + self.f_rr[i]) / Car.mass for i in range(2)] # Vector sum of f_traction, f_drag, f_rr divided by mass
+        
+    
+    def update(self): # Check at half-second intervals.
+        # Update the values of the forces, acceleration, and velocity.
+        self.f_traction = [i * self.engine_force for i in self.u] # self.u * Car.engine_force
+        self.f_drag = [i * -Car.drag_const * magnitude(self.velocity) for i in self.velocity] # drag_const * acceleration * |acceleration|
+        self.f_rr = [i * -Car.rr_const for i in self.velocity] # velocity * -rolling resistance
+        self.acceleration = [(self.f_traction[i] + self.f_drag[i] + self.f_rr[i]) / Car.mass for i in range(2)] # Vector sum of f_traction, f_drag, f_rr divided by mass
+        
+        # Get velocity (v = v + dt * a)
+        for i in range(len(self.velocity)):
+            self.velocity[i] += 0.5 * self.acceleration[i]
 
 
 root = tkinter.Tk()
@@ -52,12 +68,29 @@ root = tkinter.Tk()
 canvas = tkinter.Canvas(root, width = 600, height = 600)
 canvas.pack()
 
-"""
-car_vision = []
-for angle in (0, 45, 90, 135, 180, 225, 270, 315):
-    x_displacement = 600*cos(radians(angle))
-    y_displacement = 600*sin(radians(angle))
-    car_vision.append(canvas.create_line(car_centerX, car_centerY, car_centerX + x_displacement, car_centerY + y_displacement))
-    print(x_displacement, y_displacement)
-"""
+ae86 = Car(canvas)
+
+for _ in range(60):
+    ae86.update()
+    print()
+    print("Traction:" + str(magnitude(ae86.f_traction)))
+    print("Drag:" + str(magnitude(ae86.f_drag)))
+    print("Rolling Resistance:" + str(magnitude(ae86.f_rr)))
+    print("Acceleration:" + str(magnitude(ae86.acceleration)))
+    print("Velocity:" + str(magnitude(ae86.velocity)))
+
+    ae86.engine_force += 100 if ae86.engine_force < Car.max_engine_force else 0
+    sleep(0.1)
+while True:
+    ae86.update()
+    print()
+    print("Traction:" + str(magnitude(ae86.f_traction)))
+    print("Drag:" + str(magnitude(ae86.f_drag)))
+    print("Rolling Resistance:" + str(magnitude(ae86.f_rr)))
+    print("Acceleration:" + str(magnitude(ae86.acceleration)))
+    print("Velocity:" + str(magnitude(ae86.velocity)))
+
+    ae86.engine_force -= 100 if ae86.engine_force > 0 else 0
+    sleep(0.1)
+
 root.mainloop()
