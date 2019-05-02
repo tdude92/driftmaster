ab13e0744ed44b384a5d42b7e34c55ba33a8b044
tdude92 <41596427+tdude92@users.noreply.github.com>
2019-05-01 22:51:43 -0400

    Update evolution.py

--- evolution.py
+++ evolution.py
@@ -1,3 +1,12 @@
+from math import e
+from random import randint
+
+# Actual nodes stored in class variable. Nodes are then referenced with assigned IDs.
+
+def sigmoid(x):
+    return 1 / (1 + e ** -x)
+
+
 class OutNode:
     id_counter = 0
     nodes = []
@@ -14,6 +23,7 @@ class OutNode:
         self.value = 0
         for node in self.connections:
             self.value += InNode.nodes[node[0]].value * node[1]
+        self.value = sigmoid(self.value)
 
     def __repr__(self):
         return str(self.ID)
@@ -23,38 +33,63 @@ class InNode:
     id_counter = 0
     nodes = []
 
-    def __init__(self, value):
+    def __init__(self, boundto_var):
         self.ID = InNode.id_counter
         InNode.id_counter += 1
         InNode.nodes.append(self)
 
         self.connections = []
-        self.value = value
+        self.value = 0
+        self.bound_to = boundto_var # This is the variable that the node is bound to.
     
     def add_connection(self, node_id, weight):
         if str(node_id) in [node.__repr__() for node in OutNode.nodes]:
             self.connections.append((node_id, weight))
             OutNode.nodes[node_id].connections.append((self.ID, weight))
         else:
-            raise Exception(str(node_id) + " is an invalid node ID")
+            pass
+            #raise Exception(str(node_id) + " is an invalid node ID")
 
     def __repr__(self):
         return str(self.ID)
 
 
-if __name__ == "__main__":
-    from random import randint
+class Network:
+    def __init__(self):
+        self.input_nodes = []
+        self.output_nodes = []
+        self.output = None
     
-    out_nodes = [OutNode() for _ in range(2)]
-    in_nodes = [InNode(randint(1, 5)) for _ in range(3)]
+    def connect(self):
+        for node in self.input_nodes:
+            for outnode in self.output_nodes:
+                InNode.nodes[node].add_connection(OutNode.nodes[outnode].ID, randint(1, 10) / 10)
 
-    for node in in_nodes:
-        for outnode in out_nodes:
-            node.add_connection(outnode.ID, 2)
+    def update(self):
+        for node in self.input_nodes:
+            InNode.nodes[node].value = InNode.nodes[node].bound_to
+
+        for node in self.output_nodes:
+            OutNode.nodes[node].get_value()
+        
+        self.output = max([OutNode.nodes[node].value for node in self.output_nodes])
+
+
+if __name__ == "__main__":
+    test_network = Network()
+
+    x = 0.5
+    y = 0.4
+    z = 0.6
     
-    for i in in_nodes:
-        print(i.value)
-    
-    for i in out_nodes:
-        i.get_value()
-        print(i.value)
+    test_network.input_nodes.append(InNode(x).ID)
+    test_network.input_nodes.append(InNode(y).ID)
+    test_network.input_nodes.append(InNode(z).ID)
+
+    test_network.output_nodes.append(OutNode().ID)
+    test_network.output_nodes.append(OutNode().ID)
+
+    test_network.connect()
+    test_network.update()
+
+    print(test_network.output)
